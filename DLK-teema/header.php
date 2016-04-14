7aeced15c651959e5c24a1c157e7b141f8db7539
Ilkka Kylmäniemi <ilkkamtk@ety-a2123b-01.edu.wks.metropolia.fi>
2016-04-14 12:21:44 +0300

    update

--- DLK-teema/header.php
+++ DLK-teema/header.php
@@ -6,8 +6,8 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Foundation for Sites</title>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/foundation-flex.css">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/app.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/foundation-flex.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/app.css">
 </head>
 
 <body>
